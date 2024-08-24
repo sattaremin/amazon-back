0cf0358362205a4ec8e7b5cb9b1f80495932eda7
sattar <m.sallihemin14@gmail.com>
2024-08-24 11:59:16 -0400

    Initial commit

---
+++ src/main/java/org/cydeo/amazonback/AmazonBackApplication.java
@@ -0,0 +1,13 @@
+package org.cydeo.amazonback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cydeo/amazonback/AmazonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cydeo.amazonback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
